dbc28ae486f3b753067156f75ce36d67912cad8e
Arewafavorite AFDEC <afdecarewafavorite@gmail.com>
2026-02-02 17:05:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3601880924-22e86",
+  "appId": "1:582147304168:web:7fded798402f9d2e33775d",
+  "apiKey": "AIzaSyCl4XwksqQjAk5HAGwypqA4m962uLlR8wI",
+  "authDomain": "studio-3601880924-22e86.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "582147304168"
+};
