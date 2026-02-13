f28593bdc3030e4f549480d8df10b80e6ae7bc6e
Arewafavorite AFDEC <afdecarewafavorite@gmail.com>
2026-02-13 02:55:36 +0000

    Good. Now we solve it properly. Since you are using Android tablet only,

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "afdec",
-  "appId": "1:582147304168:web:7fded798402f9d2e33775d",
-  "apiKey": "AIzaSyCl4XwksqQjAk5HAGwypqA4m962uLlR8wI",
-  "authDomain": "afdec.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "582147304168"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "afdec",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:582147304168:web:7fded798402f9d2e33775d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCl4XwksqQjAk5HAGwypqA4m962uLlR8wI",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "afdec.firebaseapp.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "582147304168",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
