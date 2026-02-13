0994e4a4aaea45faf6b77cf9b670764e5063203c
Arewafavorite AFDEC <afdecarewafavorite@gmail.com>
2026-02-13 02:17:36 +0000

    Firebase Studio logo Firebase Studio
    AFDEC
    studio-3601880924-22e86
    
    Pub

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3601880924-22e86",
+  "projectId": "afdec",
   "appId": "1:582147304168:web:7fded798402f9d2e33775d",
   "apiKey": "AIzaSyCl4XwksqQjAk5HAGwypqA4m962uLlR8wI",
-  "authDomain": "studio-3601880924-22e86.firebaseapp.com",
+  "authDomain": "afdec.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "582147304168"
 };
